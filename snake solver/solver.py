0d2b68da6f0168cecb18f818fb56597b22c5c2d3
Paul <pslepko.ps@gmail.com>
2023-07-25 15:35:26 +0200

    first Commit

---
+++ snake solver/solver.py
@@ -0,0 +1,6 @@
+dimension = 3
+cube = [[] for i in range(3)]  # [X,Y,Z] coordinates
+direction = [] # to see the correct direction for rebuilding
+length = dimension * dimension * dimension
+print(cube)
+
