7da5ff1808a8f2bc5ce9d5eba7a3bf447fa00d30
EZP90 <pslepko.ps@gmail.com>
2023-07-26 14:27:44 +0200

    deleted hello.py as it was only a test

--- snake solver/solver.py
+++ snake solver/solver.py
@@ -1,6 +1,27 @@
 dimension = 3
+solution = False
 cube = [[] for i in range(3)]  # [X,Y,Z] coordinates
 direction = [] # to see the correct direction for rebuilding
 length = dimension * dimension * dimension
-print(cube)
+
+# print(cube)
+
+sequence = [3, 2, 2, 3, 3, 2, 3, 2, 2, 3, 2]
+
+# print(sequence)
+
+check(sequence)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
